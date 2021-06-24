989245bb42ece7ec6464b64af24425a3cb752a8f
Zaú Júlio <zauhdf@gmail.com>
2021-06-23 21:52:25 -0300

    :recycle: Remove old dir of frontend #4

--- taskiano_front/@types/User.ts
+++
@@ -1,7 +0,0 @@
-type User = {
-    id: string;
-    name: string;
-    avatar: string;
-};
-
-export default User;
--- taskiano_front/hooks/useHello.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { HelloContext } from "../contexts/HelloContext";
-
-export function useHello() {
-    return useContext(HelloContext);
-}
