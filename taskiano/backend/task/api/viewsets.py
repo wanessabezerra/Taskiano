93995695cf72278fb747d275192f426b34216066
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-07-31 17:19:25 -0300

    :memo: Atualização nos arquivos serializers.py e viewsets.py #34 #112

--- taskiano/backend/task/api/viewsets.py
+++ taskiano/backend/task/api/viewsets.py
@@ -6,3 +6,18 @@ from task import models
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TaskSerializer
     queryset = models.Task.objects.all()
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UsersSerializer
+    queryset = models.Users.objects.all()
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ProjectSerializer
+    queryset = models.Project.objects.all()
+
+
+class ReminderViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ReminderSerializer
+    queryset = models.Reminder.objects.all()
