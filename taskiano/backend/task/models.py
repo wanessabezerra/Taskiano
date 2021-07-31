0495e4e97c552affe62233b2dd1808fae927aa26
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-07-31 02:12:25 -0300

    :memo: Atualização dos atributos de user e task #34 #112 #118 #119

--- taskiano/backend/task/models.py
+++ taskiano/backend/task/models.py
@@ -1,21 +1,24 @@
-'''from django.conf import settings'''
+from django.conf import settings
 from django.db import models
 from uuid import uuid4
 
-"""
+from django.contrib.auth.models import User
 
-class User(models.Model):
 
+class Users(models.Model):
+    id_user = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True
     )
+    birthday = models.DateField(null=False)
+    email = models.EmailField(max_length=254)
+    avatar_url = models.URLField(max_length=254)
 
     def __str__(self):
-        return self.nome
-"""
+        return self.name
 
 
 class Task(models.Model):
@@ -39,19 +42,17 @@ class Task(models.Model):
     )
 
     id_task = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    note = models.TextField(max_length=255)
-    created_in = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    note = models.TextField(max_length=254)
+    created_at = models.DateTimeField(auto_now_add=True)
     closed_in = models.DateField(null=False)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     situation = models.CharField(max_length=1, choices=SITUATION_CHOICES)
     priority = models.CharField(max_length=1, choices=PRIORITIES)
+    score = models.FloatField(default=0, db_index=True)
+    user = models.ForeignKey(
+        User, related_name='%(class)ss', on_delete=models.CASCADE)
+    hashed = models.CharField(max_length=40, editable=False, db_index=True)
 
     def __str__(self):
         return self.id_task + ' - ' + self.name
-
-
-"""
-class Subtask(models.Model):
-    subtask = models.ForeignKey(Task, on_delete=models.PROTECT)
-"""
