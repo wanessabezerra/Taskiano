f307d0ef47f2d8b923e2da10a39b870cf2211c46
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-08-01 00:13:21 -0300

    :memo: Adição das rotas em Urls.py #34 #112

--- taskiano/backend/taskiano/urls.py
+++ taskiano/backend/taskiano/urls.py
@@ -1,18 +1,3 @@
-"""taskiano URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,11 +5,17 @@ from django.urls import path, include
 from rest_framework import routers
 
 from task.api import viewsets as taskviewsets
+from task.api import viewsets as usersviewsets
+from task.api import viewsets as projectviewsets
+from task.api import viewsets as reminderviewsets
 
 route = routers.DefaultRouter()
 
 route.register(r'task', taskviewsets.TaskViewSet, basename="Task")
-
+route.register(r'user', usersviewsets.UsersViewSet, basename="User")
+route.register(r'project', projectviewsets.ProjectViewSet, basename="Project")
+route.register(r'reminder', reminderviewsets.ReminderViewSet,
+               basename="Reminder")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
