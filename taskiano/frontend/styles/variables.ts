2ef1bb78741434d91895c513b4fd563f4f086dc6
zaujulio <zauhdf@gmail.com>
2021-08-04 09:27:21 -0300

    :lipstick: Add: create code vars to colors #125

---
+++ taskiano/frontend/styles/variables.ts
@@ -0,0 +1,18 @@
+const dark = "#282a36";
+const darker = "#21232d";
+const purple = "#ab70ff";
+const orange = "#ffa039";
+const hightPurple = "#832CFF";
+const hightOrange = "#FF901A";
+
+const colors = {
+  dark,
+  darker,
+  purple,
+  orange,
+  hightPurple,
+  hightOrange
+}
+
+export default colors;
+
