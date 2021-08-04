e77e28c665953f4f9b129dcd65f0067a091556e8
zaujulio <zauhdf@gmail.com>
2021-08-04 05:20:07 -0300

    :art: Add container to tasks array #125

--- taskiano/frontend/components/ProjectWidget/index.tsx
+++ taskiano/frontend/components/ProjectWidget/index.tsx
@@ -5,19 +5,19 @@ import Task from "../Task";
 import styles from "./styles.module.scss";
 
 interface ProjectWidgetProps {
-  name: string;
-  tasks: TaskType[];
+    name: string;
+    tasks: TaskType[];
 }
 
 function ProjectWidget(props: ProjectWidgetProps) {
-  return (
-    <>
-      <h1>{props.name}</h1>
-      {props.tasks.map((task, index) => {
-          return <Task key={index} {...task} />;
-      })}
-    </>
-  );
+    return (
+        <div className={styles.projectWidget}>
+            <h1>{props.name}</h1>
+            {props.tasks.map((task, index) => {
+                return <Task key={index} {...task} />;
+            })}
+        </div>
+    );
 }
 
 export default ProjectWidget;
