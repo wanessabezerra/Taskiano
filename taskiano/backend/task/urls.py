7fcc7a19b6465a00d9d282bb4ad860ed08c56f57
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-08-05 07:08:14 -0300

    :memo: Detalhes #34 #123

--- taskiano/backend/task/urls.py
+++ taskiano/backend/task/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cadastrar_tarefa', views.cadastrar_tarefa, name='cadastrar_tarefa'),
     path('detalhar_tarefa/<int:pk>/',
-         views.TarefaDetailView.as_view(), name='detalhar_tarefa'),
+         views.TaskDetailView.as_view(), name='detalhar_tarefa'),
     path('cadastrar_link', views.cadastrar_link, name='cadastrar_link'),
     path('cadastrar_tag', views.cadastrar_tag, name='cadastrar_tag'),
     path('pesquisar_tarefas', views.buscar_tarefas, name='buscar_tarefas'),
