60bfa9d90131243bbfd161ab4d4735b1643e6fea
ZauJulio <zauhdf@gmail.com>
2021-08-09 16:48:01 -0300

    :sparkles: Add: fetch with swr task's preview #125

--- taskiano/frontend/pages/home/Projects/index.tsx
+++ taskiano/frontend/pages/home/Projects/index.tsx
@@ -1,26 +1,43 @@
 import React from "react";
+import useSWR from "swr";
 
 import ProjectWidget from "../../../components/ProjectWidget";
-import { allTasks } from "../../../services/api";
+import { TaskType } from "../../../@types";
+import { api } from "../../../services/api";
 
 import styles from "./styles.module.scss";
 
 function Projects() {
-    return (
-        <div className={styles.projectsContainer}>
-            <div className={styles.projectViewOptions}>
-                <h1 className={styles.title}>Projetos</h1>
-                <span className={styles.separator} />
-            </div>
-            <div className={styles.projectsContainer}>
-                <ProjectWidget tasks={allTasks} name="UFRN" />
-                <ProjectWidget tasks={allTasks} name="Curso Técnico IMD" />
-                <ProjectWidget tasks={allTasks} name="Lista de Compras" />
-                <ProjectWidget tasks={allTasks} name="Trabalho" />
-            </div>
+  const { data } = useSWR("task/", async (u) => {
+    const res = await api.get("task/");
+    let tasks: TaskType[] = res.data;
 
-        </div>
-    );
+    for (var task of tasks) {
+      const dateInit = new Date(task.timer);
+      const dateCurr = new Date();
+
+      const diff = Number(dateInit) - Number(dateCurr);
+
+      task.remainingTime = Math.abs(diff / 1000);
+    }
+
+    return tasks;
+  });
+
+  return (
+    <div className={styles.projectsContainer}>
+      <div className={styles.projectViewOptions}>
+        <h1 className={styles.title}>Projetos</h1>
+        <span className={styles.separator} />
+      </div>
+      <div className={styles.projectsContainer}>
+        <ProjectWidget tasks={data} name="UFRN" />
+        <ProjectWidget tasks={data} name="Curso Técnico IMD" />
+        <ProjectWidget tasks={data} name="Lista de Compras" />
+        <ProjectWidget tasks={data} name="Trabalho" />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
