a27e1b9fd02dde3a2fdf3e13ce24b98008ad887b
ZauJulio <zauhdf@gmail.com>
2021-08-09 16:48:38 -0300

    :sparkles: Add: add modal wrapper to Task component #125

--- taskiano/frontend/components/Task/index.tsx
+++ taskiano/frontend/components/Task/index.tsx
@@ -1,28 +1,46 @@
-import React from "react";
-import { ClockTimer } from "../../components/ClockTimer";
+import React, { useState } from "react";
+
+import Modal from "../Modal";
+import { ClockTimer } from "../ClockTimer";
+import { MarkdownPreview } from "../MarkdownPreview";
 import { RemainsFormatValue, RemainsFormatText } from "../../utils";
 
 import styles from "./styles.module.scss";
 
 interface TaskProps {
-    id: number;
-    remainingTime: number;
-    title: string;
+  id: number;
+  title: string;
+  remainingTime: number;
+  note: string;
 }
 
 function Task({ remainingTime, ...props }: TaskProps) {
-    const description = `${RemainsFormatValue({
-        remainingTime,
-    })} ${RemainsFormatText({ remainingTime })} - ${props.title}}`;
+  const [showTasks, setShowTasks] = useState(false);
 
-    return (
-        <div className={styles.taskContainer}>
-            <div className={styles.timeContainer}>
-                <ClockTimer remainingTime={remainingTime} />
-                <h1 className={styles.timeRemains}>{description}</h1>
-            </div>
-        </div>
-    );
+  const remainsTime = RemainsFormatValue({ remainingTime });
+  const reamainsTimeText = RemainsFormatText({ remainingTime });
+
+  const description = `${remainsTime} ${reamainsTimeText} - ${props.title}`;
+
+  return (
+    <div className={styles.taskContainer}>
+      {showTasks && (
+        <Modal className={styles.modal} close={() => setShowTasks(false)}>
+          <div className={styles.taskModal}>
+            <h1 className={styles.taskModalTitle}>{props.title}</h1>
+            <MarkdownPreview
+              className={styles.taskMarkdownPreview}
+              note={props.note}
+            ></MarkdownPreview>
+          </div>
+        </Modal>
+      )}
+      <div className={styles.timeContainer} onClick={() => setShowTasks(true)}>
+        <ClockTimer remainingTime={remainingTime} />
+        <h1 className={styles.timeRemains}>{description}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Task;
