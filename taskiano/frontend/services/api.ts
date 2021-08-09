e6a25ed30800a7905ce0b7afc7d00a9090d42946
ZauJulio <zauhdf@gmail.com>
2021-08-09 16:43:13 -0300

    :sparkles: Add: add base api connect to backend #125

--- taskiano/frontend/services/api.ts
+++ taskiano/frontend/services/api.ts
@@ -1,59 +1,64 @@
 import { TaskType, Weekday } from "../@types";
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://taskiano-backend.herokuapp.com/",
+});
 
 export const nextTasks: TaskType[] = [
-    {
-        id: 1,
-        title: "Organizar repositório da disciplina de ES II",
-        remainingTime: 1800,
-    },
-    {
-        id: 2,
-        title: "Enviar tarefa BD2 Sigaa",
-        remainingTime: 120,
-    },
-    {
-        id: 3,
-        title: "Prova de Engenharia de Software 2",
-        remainingTime: 432000,
-    },
+  {
+    id: 1,
+    title: "Organizar repositório da disciplina de ES II",
+    remainingTime: 1800,
+  },
+  {
+    id: 2,
+    title: "Enviar tarefa BD2 Sigaa",
+    remainingTime: 120,
+  },
+  {
+    id: 3,
+    title: "Prova de Engenharia de Software 2",
+    remainingTime: 432000,
+  },
 ];
 
 export const lateTasks: TaskType[] = [
-    {
-        id: 1,
-        title: "Atividade de exercício BD2",
-        remainingTime: -345600,
-    },
-    {
-        id: 2,
-        title: "Tópico do fórum de BD2",
-        remainingTime: -705600,
-    },
-    {
-        id: 3,
-        title: "Responder e-mails do IMD",
-        remainingTime: -432000,
-    },
+  {
+    id: 1,
+    title: "Atividade de exercício BD2",
+    remainingTime: -345600,
+  },
+  {
+    id: 2,
+    title: "Tópico do fórum de BD2",
+    remainingTime: -705600,
+  },
+  {
+    id: 3,
+    title: "Responder e-mails do IMD",
+    remainingTime: -432000,
+  },
 ];
 
 export const allTasks: TaskType[] = [...nextTasks, ...lateTasks, ...lateTasks];
 
 export const tasks = [
-    { number: 42 },
-    { number: 48 },
-    { number: 51 },
-    { number: 57 },
-    { number: 84 },
-    { number: 93 },
-    { number: 102 },
+  { number: 42 },
+  { number: 48 },
+  { number: 51 },
+  { number: 57 },
+  { number: 84 },
+  { number: 93 },
+  { number: 102 },
 ];
 
 export const weekdays: Weekday[] = [
-    { day: "dom", count: 20 },
-    { day: "seg", count: 33 },
-    { day: "ter", count: 42 },
-    { day: "qua", count: 37 },
-    { day: "qui", count: 56 },
-    { day: "sex", count: 34 },
-    { day: "sab", count: 23 },
+  { day: "dom", count: 20 },
+  { day: "seg", count: 33 },
+  { day: "ter", count: 42 },
+  { day: "qua", count: 37 },
+  { day: "qui", count: 56 },
+  { day: "sex", count: 34 },
+  { day: "sab", count: 23 },
 ];
