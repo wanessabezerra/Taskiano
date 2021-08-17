bf7e812a8be3a9beeb5f70906d7578f08372ebde
ZauJulio <zauhdf@gmail.com>
2021-08-17 03:33:44 -0300

    :recycle: Update: update types and over name #125

--- taskiano/frontend/components/ProjectWidget/index.tsx
+++ taskiano/frontend/components/ProjectWidget/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
-import Task from "../Task";
-
-import { TaskType } from "../../@types";
-
-import styles from "./styles.module.scss";
 import ProjectStatus from "./ProjectStatus";
 import CreateTask from "../CreateTask";
+import Task from "../Task";
+
+import type { TaskType } from "../../@types";
+
+import styles from "./styles.module.scss";
 
 interface ProjectWidgetProps {
   name: string;
@@ -16,20 +16,26 @@ interface ProjectWidgetProps {
 function ProjectWidget(props: ProjectWidgetProps) {
   const [addTask, setAddTask] = useState(false);
 
+  const handleModal = {
+    openModal: () => setAddTask(true),
+    closeModal: () => setAddTask(false),
+  };
+
   return (
     <div className={styles.projectWidget}>
-      {addTask && <CreateTask close={() => setAddTask(false)} />}
+      {addTask && <CreateTask close={() => handleModal.closeModal()} />}
+
       <h1 className={styles.projectTitle}>{props.name}</h1>
       <div className={styles.projectContainer}>
         <div className={styles.projectHeader}>
           <h1 className={styles.totalTasks}>{props.tasks?.length} Tarefas</h1>
-          <ProjectStatus lates={2} timers={3} done={4} />
+          <ProjectStatus over={2} timers={3} done={4} />
         </div>
 
         <button
           type="button"
           className={styles.addTask}
-          onClick={() => setAddTask(true)}
+          onClick={() => handleModal.openModal()}
         >
           +
         </button>
