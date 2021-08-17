d05836e3aa1c44d151dd79e924c22c50b53e3e00
ZauJulio <zauhdf@gmail.com>
2021-08-17 01:42:28 -0300

    :label: Update: increase TaskType #136 #137 #138 #153 #141 #142 #148 #149 #150

--- taskiano/frontend/@types/Task.ts
+++ taskiano/frontend/@types/Task.ts
@@ -1,7 +1,12 @@
-export type TaskType = {
-  id: number;
+type TaskType = {
+  id?: number;
   title: string;
   timer?: any;
-  remainingTime: number;
+  remainingTime?: number;
   note?: string;
+  fixed?: boolean;
+  priority?: number;
+  status?: string;
 };
+
+export default TaskType;
