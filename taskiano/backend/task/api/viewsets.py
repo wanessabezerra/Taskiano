650a43c7d8bf64356016504678a6a9e4aae3b564
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-08-19 17:02:12 -0300

    :hammer: Adição de Pesquisa, Filtro e Ordenação na viwsets.py #34 #123 #141 #142

--- taskiano/backend/task/api/viewsets.py
+++ taskiano/backend/task/api/viewsets.py
@@ -1,13 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+
 from task.api import serializers
 from task import models
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 from firebase_auth.mixins import FirebaseAuthMixin
 
 
-class TaskViewSet(FirebaseAuthMixin, viewsets.ModelViewSet):
+class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TaskSerializer
     queryset = models.Task.objects.all()
+
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
 
 
 class SubTaskViewSet(FirebaseAuthMixin, viewsets.ModelViewSet):
@@ -19,10 +28,22 @@ class UsersViewSet(FirebaseAuthMixin, viewsets.ModelViewSet):
     serializer_class = serializers.UsersSerializer
     queryset = models.Users.objects.all()
 
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
+
 
 class ProjectViewSet(FirebaseAuthMixin, viewsets.ModelViewSet):
     serializer_class = serializers.ProjectSerializer
     queryset = models.Project.objects.all()
+
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
 
 
 class ReminderViewSet(FirebaseAuthMixin, viewsets.ModelViewSet):
