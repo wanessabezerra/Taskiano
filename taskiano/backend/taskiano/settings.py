84ebc78af38aa83b2bfcaf47034a6c8588441d4c
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-08-19 22:10:26 -0300

    :white_check_mark: Adição da Paginação #34 #123 #141

--- taskiano/backend/taskiano/settings.py
+++ taskiano/backend/taskiano/settings.py
@@ -157,4 +157,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination',
+    ),
+    'PAGE_SIZE': 2
 }
