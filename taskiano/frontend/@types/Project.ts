0759884b729847d626834347630e3df4f746b1b4
ZauJulio <zauhdf@gmail.com>
2021-08-22 08:53:11 -0300

    :label: Update: increase project type #125

--- taskiano/frontend/@types/Project.ts
+++ taskiano/frontend/@types/Project.ts
@@ -1,11 +1,14 @@
+import Task from "./Task";
+
 type Project = {
-  id: string;
-  name: string;
-  createDate: Date;
-  completeDate: Date;
-  description: string;
-  color: string;
-  hasArchived: boolean;
+  id?: string;
+  name?: string;
+  createDate?: Date;
+  completeDate?: Date;
+  description?: string;
+  color?: string;
+  hasArchived?: boolean;
+  tasks?: Array<Task>;
 };
 
 export default Project;
