ab2b02ec8c3a9a338480b22cfe62b4c1502dec4e
ZauJulio <zauhdf@gmail.com>
2021-08-22 09:34:36 -0300

    :sparkles: Refactor: decrease cyclomatic complexity in Carousel #125

--- taskiano/frontend/components/Carousel/PageIndicator/index.tsx
+++ taskiano/frontend/components/Carousel/PageIndicator/index.tsx
@@ -1,36 +1,31 @@
-import React, { Dispatch, ReactNode, SetStateAction } from "react";
+import React from "react";
 
 import styles from "./styles.module.scss";
 
 interface PageIndicatorProps {
-  perPage: number;
+  count: number;
   currentPage: number;
-  onClick: Dispatch<SetStateAction<number>>;
-  children: ReactNode | Element[];
+  onClick: (index: number) => void;
 }
 
 function PageIndicators(props: PageIndicatorProps) {
-  const total = React.Children.count(props.children);
-  const handleOnClick = (index: number) => props.onClick(index + 1);
+  function isCurrentPage(index: number) {
+    return index + 1 === props.currentPage;
+  }
 
   return (
     <div className={styles.pageIndicatorsContainer}>
-      {React.Children.map(props.children, (_, index) => {
-        if (index + 1 === props.currentPage)
-          return (
-            <div
-              className={styles.indicatorSelected}
-              onClick={() => handleOnClick(index)}
-            />
-          );
-        else if (index + 1 * props.perPage < total - 1)
-          return (
-            <div
-              className={styles.indicatorUnselected}
-              onClick={() => handleOnClick(index)}
-            />
-          );
-      })}
+      {Array.from({ length: props.count }, (_, index) => (
+        <div
+          key={index}
+          className={
+            isCurrentPage(index)
+              ? styles.indicatorSelected
+              : styles.indicatorUnselected
+          }
+          onClick={() => props.onClick(index + 1)}
+        />
+      ))}
     </div>
   );
 }
--- taskiano/frontend/components/Carousel/index.tsx
+++ taskiano/frontend/components/Carousel/index.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode, useState, Children, useEffect } from "react";
+import React, { ReactNode, Children, useEffect } from "react";
 
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 import PageIndicators from "./PageIndicator";
+import PageState from "./PageState";
 
 import styles from "./styles.module.scss";
 
@@ -11,36 +12,39 @@ type CarouselProps = {
   gap?: number;
   infiniteScroll?: boolean;
   autoInfiniteScroll?: boolean;
-  autoInfiniteScrollInterval?: number | 3000;
+  autoInfiniteScrollInterval?: number;
   showIndicator?: boolean;
   howMany: number;
   children: ReactNode | Element[];
 };
 
-function Carousel(props: CarouselProps) {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+function Carousel({
+  autoInfiniteScrollInterval = 3000,
+  gap = 2,
+  ...props
+}: CarouselProps) {
+  const childrenCount = Children.count(props.children);
+  const pageCount = Math.ceil(childrenCount / props.howMany);
+  const itemsContainerStyle = { gap: `${gap}rem` };
 
-  const handles = {
-    decrease() {
-      if (currentPage > 1) setCurrentPage(currentPage - 1);
-    },
-    increase() {
-      if (currentPage < Children.count(props.children) / props.howMany) {
-        setCurrentPage(currentPage + 1);
-      } else {
-        props.infiniteScroll && setCurrentPage(1);
-      }
-    },
-  };
+  const { currentPage, setCurrentPage, handles } = PageState({
+    pageCount,
+    count: childrenCount,
+    infiniteScroll: props.infiniteScroll,
+  });
+
+  function showInPage(index: number) {
+    return (
+      index + 1 <= currentPage * props.howMany &&
+      index + 1 > (currentPage - 1) * props.howMany
+    );
+  }
 
   useEffect(() => {
-    if (props.autoInfiniteScroll)
-      return () =>
-        clearTimeout(
-          setTimeout(() => handles.increase(), props.autoInfiniteScrollInterval)
-        );
+    props.autoInfiniteScroll &&
+      setTimeout(() => handles.increase(), autoInfiniteScrollInterval);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
+  }, [autoInfiniteScrollInterval, currentPage, props.autoInfiniteScroll]);
 
   return (
     <div className={`${styles.carouselContainer}  ${props?.className}`}>
@@ -49,18 +53,11 @@ function Carousel(props: CarouselProps) {
           <IoIosArrowBack onClick={handles.decrease} />
         </div>
 
-        <div
-          className={styles.itemsContainer}
-          style={{ gap: `${props.gap}rem` }}
-        >
-          {React.Children.map(props.children, (child, index) => {
-            if (
-              index + 1 <= currentPage * props.howMany &&
-              index + 1 > (currentPage - 1) * props.howMany
-            ) {
-              return child;
-            }
-          })}
+        <div className={styles.itemsContainer} style={itemsContainerStyle}>
+          {React.Children.map(
+            props.children,
+            (child, index) => showInPage(index) && child
+          )}
         </div>
 
         <div className={styles.circleIcon}>
@@ -70,12 +67,10 @@ function Carousel(props: CarouselProps) {
 
       {props.showIndicator && (
         <PageIndicators
-          perPage={props.howMany}
+          count={pageCount}
           currentPage={currentPage}
-          onClick={setCurrentPage}
-        >
-          {props.children}
-        </PageIndicators>
+          onClick={(index: number) => setCurrentPage(index)}
+        />
       )}
     </div>
   );
