86be192db95b978ff25ebc9add471aa4f4d9a59e
ZauJulio <zauhdf@gmail.com>
2021-08-22 08:45:18 -0300

    :ambulance: Hotfix: refactor api project

--- taskiano/frontend/services/api/Project.rest.ts
+++ taskiano/frontend/services/api/Project.rest.ts
@@ -1,25 +1,42 @@
 import { api } from "./";
 import type { Project } from "../../@types";
 
+interface CreateProps {
+  data: Project;
+  token?: string;
+}
+
 export const ProjectRest = {
-  async create(data: Project): Promise<Project> {
-    return api.post("/project/", data).then(
-      (res) => {
-        return res.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+  async create(props: CreateProps): Promise<Project> {
+    return api
+      .post("/project/", props.data, {
+        headers: {
+          Authorization: props.token,
+        },
+      })
+      .then(
+        (res) => {
+          return res.data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   },
   async get(id: string | undefined): Promise<Project[]> {
-    return api.get("/project/", { params: { id } }).then(
-      (res) => {
-        return res.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    return api
+      .get("/project/", {
+        headers: {
+          Authorization: id,
+        },
+      })
+      .then(
+        (res) => {
+          return res.data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   },
 };
