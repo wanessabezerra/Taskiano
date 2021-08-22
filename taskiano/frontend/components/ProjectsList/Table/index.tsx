43fe86dfb875a59151e54c73d917d588a9beefb3
ZauJulio <zauhdf@gmail.com>
2021-08-22 09:39:18 -0300

    :sparkles: Add: create new view in list for projects #125

---
+++ taskiano/frontend/components/ProjectsList/Table/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { BiLeftArrow } from "react-icons/bi";
+import { ClockTimer } from "../../ClockTimer";
+import { getDescriptionTime } from "../../../utils";
+
+import styles from "./styles.module.scss";
+
+export interface TaskProps {
+  id?: number;
+  name?: string;
+  remainingTime?: any;
+  priority: number;
+  created_at?: string;
+  project?: string;
+  projectColor?: string;
+}
+
+interface TableProps {
+  tasks: Array<TaskProps>;
+}
+
+function Table(props: TableProps) {
+  return (
+    <table className={styles.tableContent}>
+      <thead className={styles.header}>
+        <tr className={styles.row}>
+          <th className={styles.name}>Nome</th>
+          <th className={styles.timer}>Timer</th>
+          <th className={styles.priority}>Prioridade</th>
+          <th className={styles.createdAt}>Data de criação</th>
+          <th className={styles.project}>Projeto</th>
+        </tr>
+      </thead>
+
+      <tbody className={styles.body}>
+        {props.tasks.map((task, index) => (
+          <tr key={index} className={styles.row}>
+            <td className={styles.name}>{task.name}</td>
+
+            <td className={styles.timer}>
+              <ClockTimer remainingTime={task.remainingTime} />
+              {getDescriptionTime({ remainingTime: task.remainingTime })}
+            </td>
+
+            <td className={styles.priority}>{task.priority}</td>
+
+            <td className={styles.createdAt}>{task.created_at}</td>
+
+            <td className={styles.project} style={{ background: task.projectColor }}>
+              <BiLeftArrow />
+              {task.project}
+            </td>
+
+            <td className={styles.whiteSpace} />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
