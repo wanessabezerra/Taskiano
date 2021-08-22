1c7a5ee973faf64d7c967f800fabfce89e2a2bda
ZauJulio <zauhdf@gmail.com>
2021-08-22 09:17:27 -0300

    :ambulance: Fix: context provider auth #125

--- taskiano/frontend/contexts/AuthContext/Provider.ts
+++ taskiano/frontend/contexts/AuthContext/Provider.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import type { User } from "../../@types";
 
 type AuthContextType = {
-  user: User | undefined;
+  user?: User;
   signIn: (providerId: string) => Promise<void>;
   getToken: () => Promise<string | undefined>;
 };
