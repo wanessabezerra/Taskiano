ae89ee8a31edf925d5d26a3bc4e27d500050e2cf
ZauJulio <zauhdf@gmail.com>
2021-09-02 16:37:03 -0300

    :art: Update: use new context task, toast and global router #125

--- taskiano/frontend/pages/_app.tsx
+++ taskiano/frontend/pages/_app.tsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
+
+import { ToastContainer } from "react-toastify";
+import stylesToast from "../styles/Toast.module.scss";
 
 import { AuthContextProvider } from "../contexts/AuthContext";
 import { ProjectsContextProvider } from "../contexts/ProjectsContext";
+import { TasksContextProvider } from "../contexts/TasksContext";
+
+import Loader from "../components/Loader";
 
 import "../services/Firebase/initService";
 import "../styles/globals.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
   return (
-    <AuthContextProvider>
-      <ProjectsContextProvider>
-        <Component {...pageProps} />
-      </ProjectsContextProvider>
-    </AuthContextProvider>
+    <>
+      <ToastContainer theme="dark" className={stylesToast.toast} />
+      <Loader isLoading={loading} />
+
+      <AuthContextProvider router={router}>
+        <ProjectsContextProvider router={router}>
+          <TasksContextProvider>
+            <Component {...pageProps} />
+          </TasksContextProvider>
+        </ProjectsContextProvider>
+      </AuthContextProvider>
+    </>
   );
 }
 export default MyApp;
