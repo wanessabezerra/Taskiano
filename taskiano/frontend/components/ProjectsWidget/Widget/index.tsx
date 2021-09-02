78962aafc6658272ad7efc202ab38eb7e946a526
ZauJulio <zauhdf@gmail.com>
2021-09-02 16:59:46 -0300

    :art: Update: use new context project and task and general refactor #125

--- taskiano/frontend/components/ProjectsWidget/Widget/index.tsx
+++ taskiano/frontend/components/ProjectsWidget/Widget/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import ProjectStatus from "./ProjectStatus";
-import CreateTask from "../../CreateTask";
+import CreateTask from "../../Task/Create";
 import Task from "../../Task";
 
 import type { TaskType } from "../../../@types";
@@ -9,6 +9,8 @@ import type { TaskType } from "../../../@types";
 import styles from "./styles.module.scss";
 
 interface ProjectWidgetProps {
+  id?: string;
+  color?: number;
   name?: string;
   tasks?: TaskType[];
 }
@@ -21,15 +23,32 @@ function ProjectWidget(props: ProjectWidgetProps) {
     closeModal: () => setAddTask(false),
   };
 
+  const getTitle = () => {
+    if (props.tasks?.length) {
+      return `${props.tasks.length} Tarefa${props.tasks.length > 1 ? "s" : ""}`;
+    } else return "Tudo Feito";
+  };
+
   return (
     <div className={styles.projectWidget}>
-      {addTask && <CreateTask close={() => handleModal.closeModal()} />}
+      {addTask && (
+        <CreateTask
+          projectId={props.id}
+          projectColor={`#${props.color?.toString(16)}`}
+          close={() => handleModal.closeModal()}
+        />
+      )}
 
       <h1 className={styles.projectTitle}>{props.name}</h1>
       <div className={styles.projectContainer}>
         <div className={styles.projectHeader}>
-          <h1 className={styles.totalTasks}>{props.tasks?.length} Tarefas</h1>
-          <ProjectStatus over={2} timers={3} done={4} />
+          <h1 className={styles.totalTasks}>{getTitle()}</h1>
+          <ProjectStatus
+            over={2}
+            timers={3}
+            done={4}
+            color={`#${props.color?.toString(16)}`}
+          />
         </div>
 
         <button
@@ -42,13 +61,7 @@ function ProjectWidget(props: ProjectWidgetProps) {
 
         <div className={styles.tasksContainer}>
           {props.tasks?.map((task, index) => (
-            <Task
-              key={index}
-              id={task.id}
-              title={task.title}
-              remainingTime={task.remainingTime}
-              note={task.note ?? ""}
-            />
+            <Task {...task} key={task.id ?? index} />
           ))}
         </div>
       </div>
