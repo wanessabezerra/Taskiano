696e268bf4fb051b7e62216243a1084dea1c1081
ZauJulio <zauhdf@gmail.com>
2021-09-17 13:47:24 -0300

    :recycle: Refactor: general refactor authentication #141 #150

--- taskiano/backend/firebase_auth/authentication.py
+++ taskiano/backend/firebase_auth/authentication.py
@@ -1,67 +1,86 @@
 import os
 from pathlib import Path
 
-from task.models import Users
-
-from rest_framework import authentication
-from rest_framework import exceptions
-
 import firebase_admin
-from firebase_admin import credentials, auth
+from api.models import Users
+from django.core.exceptions import ObjectDoesNotExist
+from firebase_admin import credentials
+from firebase_admin.auth import (ExpiredIdTokenError, InvalidIdTokenError,
+                                 RevokedIdTokenError, verify_id_token)
+from rest_framework.authentication import (BaseAuthentication,
+                                           get_authorization_header)
+from taskiano.settings import env
 
-from .exceptions import ExpiredIdTokenError, InvalidAuthToken, RevokedIdTokenError
+from .exceptions import (AuthenticationFailedError, ExpiredIdTokenFBError,
+                         InvalidAuthTokenFBError, NoAuthTokenError,
+                         RevokedIdTokenFBError, UserAlreadyExistFBError,
+                         UserDoesNotExistFBError)
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-FIREBASE_KEY = os.path.join(
-    BASE_DIR, 'taskiano-todo-firebase-adminsdk-u16j8-6aa874af1e.json')
+
+FIREBASE_KEY = os.path.join(BASE_DIR, env('FIREBASE_KEY_PATH'))
 
 cred = credentials.Certificate(FIREBASE_KEY)
 default_app = firebase_admin.initialize_app(cred)
 
 
-class FirebaseAuthentication(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        auth_header = request.META.get('HTTP_AUTHORIZATION')
+class FirebaseAuthentication(BaseAuthentication):
+
+    def __getToken(self, request: any) -> str:
+        auth = get_authorization_header(request).split()
+
+        if not auth:
+            raise NoAuthTokenError()
+        if len(auth) == 2:
+            if auth[1]:
+                auth = auth[1]
+        else:
+            auth = auth[0]
+
+        try:
+            decoded_token = verify_id_token(auth)
+        except RevokedIdTokenError:
+            raise RevokedIdTokenFBError()
+        except ExpiredIdTokenError:
+            raise ExpiredIdTokenFBError()
+        except InvalidIdTokenError:
+            raise InvalidAuthTokenFBError()
+        except Exception:
+            raise AuthenticationFailedError()
+
+        return decoded_token
+
+    def __isCreateRoute(self, request: any) -> bool:
         method = request.META.get('REQUEST_METHOD')
         route = request.META.get('PATH_INFO')
 
-        if auth_header and ' ' in auth_header:
-            auth_header = auth_header.split(' ')[1]
+        return method == "POST" and route == "/users/"
 
-        if method == "POST" and route == "/users/":
+    def __createUser(self, request: any):
+        return Users.objects.create(
+            id=request.data.get('id'),
+            name=request.data.get('name'),
+            avatar=request.data.get('avatar'),
+            email=request.data.get('email'),
+        )
+
+    def authenticate(self, request):
+        token = self.__getToken(request)
+
+        if self.__isCreateRoute(request):
             try:
-                user = Users.objects.get(id=request.data.get('id'))
-                return ((), None)
-            except Users.DoesNotExist:
-                user = Users.objects.create(
-                    id=request.data.get('id'),
-                    name=request.data.get('name'),
-                    avatar=request.data.get('avatar'),
-                    email=request.data.get('email'),
-                )
+                Users.objects.get(id=request.data.get('id'))
+                raise UserAlreadyExistFBError()
+            except ObjectDoesNotExist:
+                user = self.__createUser(request)
 
                 Users.is_authenticated = True
                 return (user, None)
 
         try:
-            decoded_token = auth.verify_id_token(auth_header)
-        except auth.RevokedIdTokenError:
-            raise RevokedIdTokenError
-        except auth.ExpiredIdTokenError:
-            raise ExpiredIdTokenError
-        except auth.InvalidIdTokenError:
-            raise InvalidAuthToken
-
-        if not auth_header or not decoded_token:
-            raise InvalidAuthToken
-
-        uid = decoded_token.get('uid')
-
-        try:
-            user = Users.objects.get(id=uid)
+            user = Users.objects.get(id=token.get('uid'))
         except Users.DoesNotExist:
-            raise exceptions.AuthenticationFailed('The user does not exist')
+            raise UserDoesNotExistFBError()
 
         Users.is_authenticated = True
-        return (user, auth_header)
+        return (user, None)
