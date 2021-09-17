e0079df9e6842997213da7ed105f972dd5ba8c49
ZauJulio <zauhdf@gmail.com>
2021-09-17 14:02:11 -0300

    :art: Update: set correction for backend endpoint

--- taskiano/frontend/services/api/index.ts
+++ taskiano/frontend/services/api/index.ts
@@ -8,6 +8,6 @@ import { HistoryRest } from "./History.rest";
 export { UserRest, TaskRest, ProjectRest, HistoryRest };
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:4000/",
-  // baseURL: "https://taskiano-backend.herokuapp.com/",
+  // baseURL: "http://127.0.0.1:4000/",
+  baseURL: "https://taskiano-backend.herokuapp.com/",
 });
