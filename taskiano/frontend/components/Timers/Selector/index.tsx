fa6ca0ef1ec87a1f986b22830e9bec5efbf24327
ZauJulio <zauhdf@gmail.com>
2021-09-17 13:26:10 -0300

    :art: Update: increase Timers to use context's #125

--- taskiano/frontend/components/Timers/Selector/index.tsx
+++ taskiano/frontend/components/Timers/Selector/index.tsx
@@ -1,23 +1,50 @@
-import React from "react";
-import { VscTriangleLeft, VscTriangleRight } from "react-icons/vsc";
-
+import React, { Children, ReactNode, useEffect, useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import styles from "./styles.module.scss";
 
+interface SelectorProps {
+  onChange: (value: number) => void;
+  current?: number;
+  children: ReactNode[];
+}
 
-function Selector() {
-    return (
-        <div className={styles.selector}>
-            <div className={styles.selectorItem}>
-                <VscTriangleLeft />
-            </div>
+function Selector(props: SelectorProps) {
+  const [selected, setSelected] = useState(props.current ?? 0);
+  const childrenCount = Children.count(props.children);
 
-            <h1 className={styles.projectName}>UFRN</h1>
+  const handles = {
+    decrease: () => {
+      if (selected === 0) setSelected(childrenCount - 1);
+      else setSelected(selected - 1);
+    },
+    increase: () => {
+      if (selected === childrenCount - 1) setSelected(0);
+      else setSelected(selected + 1);
+    },
+  };
 
-            <div className={styles.selectorItem}>
-                <VscTriangleRight />
-            </div>
+  useEffect(() => {
+    props.onChange(selected);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected]);
+
+  return (
+    <div className={styles.selector}>
+      {childrenCount > 1 && (
+        <div className={styles.circleIcon} onClick={handles.decrease}>
+          <IoIosArrowBack />
         </div>
-    );
+      )}
+
+      <div className={styles.children}>{props.children[selected]}</div>
+
+      {childrenCount > 1 && (
+        <div className={styles.circleIcon} onClick={handles.increase}>
+          <IoIosArrowForward />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Selector;
--- taskiano/frontend/components/Timers/index.tsx
+++ taskiano/frontend/components/Timers/index.tsx
@@ -1,57 +1,80 @@
 import React from "react";
+import dynamic from "next/dynamic";
 
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
-
-import Compass from "../Compass";
 import Selector from "./Selector";
+import Compass from "../Compass";
 import RenderTime from "./RenderTime";
 import Carousel from "../Carousel";
 
-import { tasks, weekdays } from "../../services/api";
+import useTimersState from "./useTimersState";
 
+import type { TaskType } from "../../@types";
 import colors from "../../styles/colors";
 import styles from "./styles.module.scss";
 
+const CountdownCircleTimer = dynamic(
+  () => import("../CountdownCircleTimerWrapper"),
+  { ssr: false }
+);
+
 function Timers() {
+  const state = useTimersState();
+
+  const isActive = (task: TaskType) => {
+    return task?.number === state.selectedTask?.number;
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.projectSelector}>
-        <h1 className={styles.title}>Projeto</h1>
-        <Selector />
+      <div className={styles.countdown}>
+        <div className={styles.projectSelector}>
+          <Selector
+            onChange={state.handleSelectProject}
+            current={state.currentProjectIndex}
+          >
+            {state.currentProjects.map((project) => (
+              <option className={styles.selectorOption} key={project.id}>
+                {project.name}
+              </option>
+            ))}
+          </Selector>
+        </div>
+
+        <h1 className={styles.title}>{state.selectedTask?.title}</h1>
+
+        <CountdownCircleTimer
+          isPlaying
+          isLinearGradient
+          key={state.selectedTask?.id}
+          duration={state?.duration}
+          initialRemainingTime={state.initialRemainingTime}
+          colors={[
+            [colors.highOrange, 0],
+            [colors.highPurple, 1],
+          ]}
+        >
+          {RenderTime}
+        </CountdownCircleTimer>
+
+        <Carousel howMany={3} infiniteScroll>
+          {state.currentTasks?.map((task) => (
+            <a
+              key={task.id}
+              className={styles.taskTag}
+              onClick={() => state.handleSelectTask(task?.number)}
+              style={{ color: isActive(task) ? colors.orange : colors.white }}
+            >
+              #{task.number}
+            </a>
+          ))}
+        </Carousel>
       </div>
-
-      <h1 className={styles.countdownTitle}>Timer + Próximo</h1>
-
-      <div className={styles.countdownDescription}>
-        Organizar repositório da disciplina...
-      </div>
-
-      <CountdownCircleTimer
-        isPlaying
-        isLinearGradient
-        duration={60}
-        colors={[
-          [colors.highOrange, 0],
-          [colors.highPurple, 1],
-        ]}
-        onComplete={() => [true, 1000]}
-      >
-        {RenderTime}
-      </CountdownCircleTimer>
-
-      <Carousel howMany={3} infiniteScroll>
-        {tasks.map((task, index) => (
-          <a key={task.number} className={styles.taskTag}>
-            #{task.number}
-          </a>
-        ))}
-      </Carousel>
 
       <span className={styles.sepLine} />
 
       <div className={styles.compass}>
         <h1 className={styles.compassTitle}>Compasso</h1>
-        <Compass weekdays={weekdays} />
+        <Compass />
       </div>
     </div>
   );
