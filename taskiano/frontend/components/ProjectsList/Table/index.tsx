5f68a3f76332082cf94d70cb8346d8048717e515
ZauJulio <zauhdf@gmail.com>
2021-09-17 13:32:26 -0300

    :art: Update: apply new context's to ProjectTable #125

--- taskiano/frontend/components/ProjectsList/Table/index.tsx
+++ taskiano/frontend/components/ProjectsList/Table/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { BiLeftArrow } from "react-icons/bi";
 import { ClockTimer } from "../../ClockTimer";
@@ -6,8 +6,10 @@ import { getDescriptionTime } from "../../../utils";
 
 import Task from "../../Task";
 
-import type { TaskType } from "../../../@types";
+import type { Project, TaskType } from "../../../@types";
 import styles from "./styles.module.scss";
+import EditorProject from "../../Project/Editor";
+import { useProjects } from "../../../hooks/useProjects";
 
 export interface TaskProps extends TaskType {
   projectName?: string;
@@ -20,47 +22,72 @@ interface TableProps {
 }
 
 function Table(props: TableProps) {
+  const [editProject, setEditProject] = useState(false);
+  const [selectedProject, setSelectedProject] = useState<Project>();
+
+  const getProject = useProjects((ctx) => ctx.get);
+
+  const EditorProjectView = () => {
+    if (editProject && selectedProject)
+      return (
+        <EditorProject
+          project={selectedProject}
+          close={() => setEditProject(false)}
+        />
+      );
+
+    return null;
+  };
+
   return (
-    <table className={styles.tableContent}>
-      <thead className={styles.header}>
-        <tr className={styles.row}>
-          <th className={styles.name}>Nome</th>
-          <th className={styles.timer}>Timer</th>
-          <th className={styles.priority}>Prioridade</th>
-          <th className={styles.createdAt}>Data de criação</th>
-          <th className={styles.project}>Projeto</th>
-        </tr>
-      </thead>
+    <>
+      <EditorProjectView />
 
-      <tbody className={styles.body}>
-        {props.tasks?.map((task) => (
-          <tr key={task?.id} className={styles.row}>
-            <td className={styles.name}>
-              <Task hideTimer key={task?.id} {...task} />
-            </td>
-
-            <td className={styles.timer}>
-              <ClockTimer remainingTime={task?.remainingTime} />
-              {getDescriptionTime(task?.remainingTime)}
-            </td>
-
-            <td className={styles.priority}>{task?.priority}</td>
-
-            <td className={styles.createdAt}>{task?.created_at}</td>
-
-            <td
-              className={styles.project}
-              style={{ background: task?.projectColor }}
-            >
-              <BiLeftArrow />
-              {task?.projectName}
-            </td>
-
-            <td className={styles.whiteSpace} />
+      <table className={styles.tableContent}>
+        <thead className={styles.header}>
+          <tr className={styles.row}>
+            <th className={styles.name}>Nome</th>
+            <th className={styles.timer}>Timer</th>
+            <th className={styles.priority}>Prioridade</th>
+            <th className={styles.createdAt}>Data de criação</th>
+            <th className={styles.project}>Projeto</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody className={styles.body}>
+          {props.tasks?.map((task) => (
+            <tr key={task?.id} className={styles.row}>
+              <td className={styles.name}>
+                <Task hideTimer key={task?.id} {...task} />
+              </td>
+
+              <td className={styles.timer}>
+                <ClockTimer remainingTime={task?.remainingTime} />
+                {getDescriptionTime(task?.remainingTime)}
+              </td>
+
+              <td className={styles.priority}>{task?.priority}</td>
+
+              <td className={styles.createdAt}>{task?.created_at}</td>
+
+              <td
+                className={styles.project}
+                style={{ background: task?.projectColor }}
+                onClick={() => {
+                  setEditProject(true);
+                  setSelectedProject(getProject(task?.projectId));
+                }}
+              >
+                <BiLeftArrow />
+                {task?.projectName}
+              </td>
+
+              <td className={styles.whiteSpace} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
