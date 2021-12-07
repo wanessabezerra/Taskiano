69a2a2121551bfad58902da4fe9456b08536d64f
ZauJulio <zauhdf@gmail.com>
2021-12-07 09:36:47 -0300

    :recycle: Remove python backend

--- taskiano/backend/firebase_auth/mixins.py
+++
@@ -1,6 +0,0 @@
-from .authentication import FirebaseAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-class FirebaseAuthMixin():
-    permission_classes = (IsAuthenticated,)
-    authentication_classes = (FirebaseAuthentication,)
