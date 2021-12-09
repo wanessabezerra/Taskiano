74716c7d4455bc54d54324aeba54d32f3a93ad99
ZauJulio <zauhdf@gmail.com>
2021-12-09 16:40:16 -0300

    :recycle: Move global sytles to new dir

---
+++ taskiano/styles/colors.ts
@@ -0,0 +1,41 @@
+type Color = string;
+
+const blueDark: Color = "#24283f";
+const dark: Color = "#282a36";
+const darker: Color = "#21232d";
+const purple: Color = "#ab70ff";
+const orange: Color = "#ffa039";
+const highPurple: Color = "#832CFF";
+const highOrange: Color = "#FF901A";
+const gray: Color = "#797979";
+const whiteGray: Color = "#d8d8d8";
+const white: Color = "#f1f1f1";
+const darkForce: Color = "#111218";
+const red = "#ff6363";
+const orangeDark = "#ff7055";
+const gold = "#ff8f36";
+const yellow = "#eab521";
+const cyan = "#8be9fd";
+const green = "#3ae651";
+
+const colors = {
+  dark,
+  darker,
+  darkForce,
+  purple,
+  orange,
+  highPurple,
+  highOrange,
+  gray,
+  white,
+  whiteGray,
+  blueDark,
+  red,
+  orangeDark,
+  gold,
+  yellow,
+  cyan,
+  green,
+};
+
+export default colors;
