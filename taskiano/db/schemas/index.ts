0c107d2be60b7826f184f40e1dff742d2766af8a
ZauJulio <zauhdf@gmail.com>
2021-12-09 16:23:03 -0300

    :sparkles: Add models schemas

---
+++ taskiano/db/schemas/index.ts
@@ -0,0 +1,6 @@
+import HistorySchema from "./HistorySchema";
+import ProjectSchema from "./ProjectSchema";
+import TaskSchema from "./TaskSchema";
+import UserSchema from "./UserSchema";
+
+export { UserSchema, TaskSchema, ProjectSchema, HistorySchema };
